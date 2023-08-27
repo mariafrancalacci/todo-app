9c4c64d6c1432b95c1859e1adb6d965c27f91e0b
Malu <malu@MacBook-Pro-de-Joao.local>
2023-08-27 19:50:12 +0100

    first commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,8 @@ Vue.use(Vuex);
 interface Todo {
   id: number;
   text: string;
-  completed: boolean;
+  isCompleted: boolean;
+  isEditing: boolean;
 }
 
 interface State {
@@ -17,49 +18,23 @@ const state: State = {
   todos: [
     {
       id: 1,
-      text: 'cozinhar',
-      completed: false
+      text: 'task',
+      isCompleted: false,
+      isEditing: false
     }
   ]
 };
 
 const mutations = {
-  addTodo: (state: State, todo: Todo) => {
-    state.todos.push(todo);
-  },
-  removeTodo: (state: State, todoId: number) => {
-    const index = state.todos.findIndex(todo => todo.id === todoId);
-    if (index !== -1) {
-      state.todos.splice(index, 1);
-    }
-  },
-  toggleTodoCompletion: (state: State, todoId: number) => {
-    const todo = state.todos.find(todo => todo.id === todoId);
-    if (todo) {
-      todo.completed = !todo.completed;
-    }
-  }
+  
 };
 
 const actions = {
-  addTodo: ({ commit }: any, todoText: string) => {
-    const newTodo: Todo = {
-      id: Date.now(),
-      text: todoText,
-      completed: false
-    };
-    commit('addTodo', newTodo);
-  },
-  removeTodo: ({ commit }: any, todoId: number) => {
-    commit('removeTodo', todoId);
-  },
-  toggleTodoCompletion: ({ commit }: any, todoId: number) => {
-    commit('toggleTodoCompletion', todoId);
-  }
+  
 };
 
 const getters = {
-  // Define your getters here
+
 };
 
 const store = new Vuex.Store<State>({
