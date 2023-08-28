4be1c65ab1580a04dff808249f72b4a954f3898f
mariafrancalacci <malu.cartier@outlook.com>
2023-08-28 14:41:29 +0100

    Added icon framework

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
-import  store  from './store'; 
+import store from './store/modules/index';
+import { Icon } from '@iconify/vue2';
+
+Vue.component('Icon', Icon);
+
 
 new Vue({
   store,
   render: h => h(App),
-}).$mount('#app'); 
+}).$mount('#app');
