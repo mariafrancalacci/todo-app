ff72411ea0924f27c3d3325e8e848dcae1a1ad86
mariafrancalacci <malu.cartier@outlook.com>
2023-08-28 14:42:26 +0100

    Added function to create new todo to Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,43 @@
 <template>
    <header class="bg-gray-700 flex items-center justify-center h-[12.5rem] relative">
     <h1 class=" text-gray-200">TODO LIST</h1>
-      <form class="absolute h-[3.375rem] bottom-[calc(-3.375rem_/_2)] w-full max-w-[46rem] flex gap-2 px-4 py-0">
+      <form @submit.prevent="createTodo" class="absolute h-[3.375rem] bottom-[calc(-3.375rem_/_2)] w-full max-w-[46rem] flex gap-2 px-4 py-0">
         <input
-          placeholder="Add a new task"
           type="text"
+          v-model="newTodo" 
+          placeholder="Enter a new task" 
           class="h-full flex-1 placeholder-gray-300 text-gray-100 border text-base px-4 py-0 rounded-lg border-solid border-gray-700 bg-gray-500"
         />
         
         <button class="h-full text-gray-100 flex items-center gap-1.5 font-bold text-sm px-4 py-0 rounded-lg border-[none] bg-blue-dark">
           Create
+          <Icon icon="clarity:plus-circle-line" color="#f2f2f2" width="18" height="18" />
         </button>
       </form>
    </header>
 </template>
 
-<script setup lang="ts">
 
+<script lang="ts">
+import Vue from 'vue';
+import { mapActions } from 'vuex';
+
+export default Vue.extend({
+  data() {
+    return {
+      newTodo: '',
+    };
+  },
+  methods: {
+    ...mapActions('todos', ['addTodo']),
+    createTodo() {
+      if (this.newTodo.trim() !== '') {
+      this.addTodo({ text: this.newTodo, isCompleted: false, dueDate: null });
+      this.newTodo = '';
+    }
+    },
+  },
+});
 </script>
+
+
