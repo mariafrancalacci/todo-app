44b92f63a7573f6fce81d6f233e18d9b40fc95e1
mariafrancalacci <malu.cartier@outlook.com>
2023-08-28 17:33:15 +0100

    Added date field

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,21 @@
 <template>
    <header class="bg-gray-700 flex items-center justify-center h-[12.5rem] relative">
     <h1 class=" text-gray-200">TODO LIST</h1>
-      <form @submit.prevent="createTodo" class="absolute h-[3.375rem] bottom-[calc(-3.375rem_/_2)] w-full max-w-[46rem] flex gap-2 px-4 py-0">
-        <input
+      <form @submit.prevent="createTodo" class="absolute h-[3.375rem] bottom-[calc(-3.375rem_/_2)] w-full max-w-[46rem] flex gap-2 px-4 py-0 justify-between">
+        <div class="flex gap-2">
+          <input
           type="text"
-          v-model="newTodo" 
+          v-model="newTodoText" 
           placeholder="Enter a new task" 
-          class="h-full flex-1 placeholder-gray-300 text-gray-100 border text-base px-4 py-0 rounded-lg border-solid border-gray-700 bg-gray-500"
+          class="h-full w-[430px] placeholder-gray-300 text-gray-100 border text-base px-4 py-0 rounded-lg border-solid border-gray-700 bg-gray-500"
         />
+
+        <input
+          type="date"
+          v-model="newTodoDueDate"
+          class="h-full w-[160px] text-gray-300 border text-base px-4 py-0 rounded-lg border-solid border-gray-700 bg-gray-500"
+        />
+        </div>
         
         <button class="h-full text-gray-100 flex items-center gap-1.5 font-bold text-sm px-4 py-0 rounded-lg border-[none] bg-blue-dark">
           Create
@@ -21,23 +29,32 @@
 <script lang="ts">
 import Vue from 'vue';
 import { mapActions } from 'vuex';
+import { Task } from '@/store/modules/todos';
 
 export default Vue.extend({
   data() {
     return {
-      newTodo: '',
+      newTodoText: '',
+      newTodoDueDate: '',
     };
   },
   methods: {
     ...mapActions('todos', ['addTodo']),
     createTodo() {
-      if (this.newTodo.trim() !== '') {
-      this.addTodo({ text: this.newTodo, isCompleted: false, dueDate: null });
-      this.newTodo = '';
+      if (this.newTodoText.trim() !== '' && this.newTodoDueDate) {
+        const newTask: Task = {
+          id: 0,
+          text: this.newTodoText,
+          isCompleted: false,
+          dueDate: new Date(this.newTodoDueDate),
+        };
+        this.addTodo(newTask);
+        this.newTodoText = '';
+        this.newTodoDueDate = '';
+      }
     }
-    },
   },
-});
+})
 </script>
 
 
