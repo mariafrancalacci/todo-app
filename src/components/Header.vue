9549a6f00b056552b0d4ead0d9df845758df3219
mariafrancalacci <malu.cartier@outlook.com>
2023-08-30 14:36:46 +0100

    update responsive css

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,27 @@
 <template>
   <header class="bg-gray-700 flex items-center justify-center h-[12.5rem] relative">
-    <h1 class=" text-gray-200">TODO LIST</h1>
+    <div class="flex gap-2">
+      <h1 class=" text-blue font-weight: 700 text-xl ">TODO</h1>
+      <h1 class=" text-purple font-weight: 700 text-xl ">LIST</h1>
+      <Icon icon="lucide:list" color="#FFFFFF" width="30" height="30" />
+    </div>
     <form @submit.prevent="createTodo"
-      class="absolute h-[3.375rem] bottom-[calc(-3.375rem_/_2)] w-full max-w-[46rem] flex gap-2 px-4 py-0 justify-between">
-      <div class="flex gap-2">
+      class="absolute h-[3.375rem] bottom-[calc(-3.375rem_/_2)] w-full max-w-[46rem] flex flex-col md:flex-row md:gap-2 px-4 py-0 justify-between">
+      <div class="flex gap-[1.5rem] md:gap-2">
         <input type="text" v-model="newTodoText" placeholder="Enter a new task" data-testid="new-todo-title"
-          :class="[isFailure && newTodoText.length === 0 ? 'h-full w-[430px] placeholder-gray-300 text-gray-100 border text-base px-4 py-0 rounded-lg border-solid border-red-800 bg-gray-500' : 'h-full w-[430px] placeholder-gray-300 text-gray-100 border text-base px-4 py-0 rounded-lg border-solid border-gray-700 bg-gray-500']" />
+          :class="[isFailure && newTodoText.length === 0 ? 'h-full w-[300px] sm:w-[430px] placeholder-gray-300 text-gray-100 border text-base px-4 py-0 rounded-lg border-solid border-red-800 bg-gray-500' : 'h-full w-[300px] sm:w-[430px] placeholder-gray-300 text-gray-100 border text-base px-4 py-0 rounded-lg border-solid border-gray-700 bg-gray-500']" />
 
         <input type="date" v-model="newTodoDueDate" data-testid="new-todo-date"
-          :class="[isFailure && newTodoDueDate.length === 0 ? 'h-full w-[160px] text-gray-300 border text-base px-4 py-0 rounded-lg border-solid border-red-800 bg-gray-500' : 'h-full w-[160px] text-gray-300 border text-base px-4 py-0 rounded-lg border-solid border-gray-700 bg-gray-500']" />
+          :class="[isFailure && newTodoDueDate.length === 0 ? 'h-full  w-[55px] sm:w-[160px] text-gray-300 border text-base px-4 py-0 rounded-lg border-solid border-red-800 bg-gray-500' : 'h-full  w-[55px] sm:w-[160px] text-gray-500  sm:text-gray-300 border text-base px-4 py-0 rounded-lg border-solid border-gray-700 bg-gray-500']" />
       </div>
 
-      <button data-testid="new-todo-submit"
-        class="h-full text-gray-100 flex items-center gap-1.5 font-bold text-sm px-4 py-0 rounded-lg border-[none] bg-blue-dark">
-        Create
-        <Icon icon="clarity:plus-circle-line" color="#f2f2f2" width="18" height="18" />
-      </button>
+      <div class="flex justify-end">
+        <button data-testid="new-todo-submit"
+          class="h-full text-gray-100 w-[100px] flex items-center gap-1.5  mt-2 md:mt-0 font-bold text-sm px-4 py-0 rounded-lg border-[none] bg-blue-dark">
+          Create
+          <Icon icon="clarity:plus-circle-line" color="#f2f2f2" width="18" height="18" />
+        </button>
+      </div>
     </form>
   </header>
 </template>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full border bg-gray-500 border-gray-400 flex items-center justify-between gap-3 p-[1rem] rounded-lg border-solid">
+    class="w-full border bg-gray-500 border-gray-400 flex md:items-center md:justify-between gap-3 p-[1rem] rounded-lg border-solid">
 
     <button data-testid="complete-todo-button" :data-checked="task.isCompleted"
       class="w-[1.125rem] h-[1.125rem] border-none bg-none" @click="handleTodoComplete">
@@ -9,54 +9,68 @@
     </button>
 
     <template v-if="editing" class="flex p-[0.5rem]">
-      <input type="text" v-model="task.text" data-testid="edit-todo-title"
-        :class="[isTitleEmpty ? 'border-solid border border-red-800  text-gray-100 text-base px-4 py-0 rounded-lg bg-gray-500' : ' placeholder-gray-300 text-gray-100 text-base px-4 py-0 rounded-lg bg-gray-500']" />
+      <div class="flex flex-col gap-2 md:flex-row">
+        <div class="flex justify-between gap-2 md:gap-[0.5rem]">
+          <input type="text" v-model="task.text" data-testid="edit-todo-title"
+            :class="[isTitleEmpty ? 'border-solid border md:w-[400px] border-red-800 w-[250px] text-gray-100 text-base px-4 py-0 rounded-lg bg-gray-500' : ' placeholder-gray-300 border border-gray-600 w-[250px] md:w-[400px] text-gray-100 text-base pl-1 pr-4 md:px-4 py-0 rounded-lg bg-gray-500']" />
 
-      <input v-if="editing" type="date" v-model="task.dueDate" data-testid="edit-todo-date"
-        class="text-gray-300  text-sm border py-1 pl-1 rounded-lg border-solid border-gray-700 bg-gray-500" />
+          <input v-if="editing" type="date" v-model="task.dueDate" data-testid="edit-todo-date"
+            class="text-gray-300  text-sm border py-[0.1rem] md:py-1 md:pl-1 rounded-lg border-solid border-gray-700 bg-gray-500 w-[25px] md:w-[130px] pr-1" />
+        </div>
 
-      <div class="pl-0 flex gap-2">
-        <button data-testid="edit-todo-confirm-button"
-          class="text-sm  border-green-700 rounded-lg border py-1 px-1 border-solid " @click="saveEdit">
-          Save
-        </button>
+        <div class="pl-0 flex gap-2 flex justify-end">
+          <button data-testid="edit-todo-confirm-button"
+            class="text-sm  border-green-700 rounded-lg border py-1 px-1 border-solid " @click="saveEdit">
+            Save
+          </button>
 
-        <button class="text-sm rounded-lg border py-1 px-1 border-solid border-red-700"
-          @click="cancelEdit">Cancel</button>
+          <button class="text-sm rounded-lg border py-1 px-1 border-solid border-red-700"
+            @click="cancelEdit">Cancel</button>
+        </div>
       </div>
     </template>
     <template v-else>
-      <p data-testid="todo-element" :class="[task.isCompleted ? 'text-gray-300 line-through text-sm leading-[1.18rem] mr-auto' : 'text-sm leading-[1.18rem] text-gray-100 mr-auto',
+      <p data-testid="todo-element" :class="[task.isCompleted ? 'text-gray-300 line-through text-sm ml-0 leading-[1.18rem] mr-auto' : 'text-sm leading-[1.18rem] text-gray-100 mr-auto',
       isOverdue() && !task.isCompleted ? 'text-red-500' : '']">
         {{ task.text }}
       </p>
     </template>
 
-    <div :class="[editing ? 'text-xs flex items-center gap-1' : 'flex items-center gap-1']">
-      <Icon icon="ph:clock-light" color="#808080" width="19" height="19" />
-      <span>{{ formatDueDate(task.dueDate) }}</span>
-    </div>
-    <button data-testid="edit-todo-button" class="text-gray-300 border-none" @click="startEdit">
-      <Icon icon="material-symbols:edit-outline" color="#808080" />
-    </button>
-    <button data-testid="delete-todo-prompt-button" @click="showModal()" class="text-gray-300 border-none">
-      <Icon icon="octicon:trash-24" color="#808080" />
-    </button>
-    <button data-testid="duplicate-todo-button" @click="multiplyTodo" class="text-gray-300 border-none">
-      <Icon icon="ion:duplicate-outline" color="#808080" />
-    </button>
 
-
-    <div data-testid="delete-todo-modal" v-if="showDeleteModal"
-      class="fixed inset-0 flex items-center justify-center z-50">
-      <div class=" bg-gray-500 rounded-lg p-4">
-        <p>Are you sure you want to delete this task?</p>
-        <div class="flex justify-end mt-4">
-          <button data-testid="delete-todo-cancel-button" @click="unShowModal" class="text-gray-500 mr-2">Cancel</button>
-          <button data-testid="delete-todo-confirm-button" @click="handleRemoveTodo" class="text-red-500">Delete</button>
-        </div>
+    <template v-if="!editing">
+      <div class="flex items-center gap-1 md:min-w-[170px]">
+        <Icon icon="ph:clock-light" color="#808080" width="19" height="19" />
+        <span>{{ formatDueDate(task.dueDate) }}</span>
       </div>
-    </div>
+      <button data-testid="edit-todo-button" class="text-gray-300 border-none" @click="startEdit">
+        <Icon icon="material-symbols:edit-outline" color="#808080" />
+      </button>
+      <button data-testid="delete-todo-prompt-button" @click="showModal()" class="text-gray-300 border-none">
+        <Icon icon="octicon:trash-24" color="#808080" />
+      </button>
+      <button data-testid="duplicate-todo-button" @click="multiplyTodo" class="text-gray-300 border-none">
+        <Icon icon="ion:duplicate-outline" color="#808080" />
+      </button>
+    </template>
+
+
+    <template>
+      <transition>
+        <div data-testid="delete-todo-modal" v-if="showDeleteModal"
+          class="fixed inset-0 flex items-center justify-center z-50">
+          <div class="fixed inset-0 backdrop-blur-md"></div>
+          <div class="bg-gray-500 rounded-lg p-4 relative">
+            <p>Are you sure you want to delete this task?</p>
+            <div class="flex justify-end mt-4">
+              <button data-testid="delete-todo-cancel-button" @click="unShowModal"
+                class="text-blue-dark mr-2">Cancel</button>
+              <button data-testid="delete-todo-confirm-button" @click="handleRemoveTodo"
+                class="text-red-500 ">Delete</button>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </template>
 
   </div>
 </template>
